55780797e11565d5f531914c5ef29eb5056d0b29
Montag55 <lucas.huebner@uni-weimar.de>
2019-06-07 14:03:44 +0200

    added tcp-recieving-server

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,61 @@
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 int main(int argc, char **argv) {
 
-    std::cout << "Major version : " << CV_MAJOR_VERSION << std::endl;
-    std::cout << "Minor version : " << CV_MINOR_VERSION << std::endl;
-    std::cout << "OpenCV version : " << CV_VERSION << std::endl;
-    std::cout << "Subminor version : " << CV_SUBMINOR_VERSION << std::endl;
-    std::cout << "Build info : " << cv::getBuildInformation().c_str() << std::endl;
-
-    if (argc != 2)
-    {
-        printf("usage: $./Quality <Image_Path>\n");
+    if (argc != 3){
+        printf("usage: $./HandSee <server-IP> <server-Port>\n");
         return -1;
     }
 
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    const char* server_ip = argv[1];
+    const long port = std::stol(argv[2]);
+    char buffer[1024] = {0};
 
-    cv::Mat image = cv::imread(argv[1], 1);
-    if (!image.data) {
-        printf("No image data \n");
-        return -1;
-    } 
-    else {
-        cv::imwrite("test.jpg", image);
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        std::cout << "socket failed" << std::endl;
+        exit(EXIT_FAILURE);
     }
-    
-    return 0;
+
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        std::cout << "setsockopt" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( port );
+
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        std::cout << "bind failed" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0) {
+        std::cout << "listen" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+        std::cout << "accept" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    while(valread = read( new_socket , buffer, sizeof(buffer)) > 0 ){
+      printf("%s\n",buffer );
+    }
+
+    return EXIT_FAILURE;
 }
